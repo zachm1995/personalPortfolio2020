fb6a86f8bcf93f2769d05974dff5ec155aef12c1
Zach Mumbauer <mumbeezy@gmail.com>
2020-02-13 22:34:55 -0500

    built out navigation

--- app/javascript/packs/components/header/navigation/Navigation.jsx
+++ app/javascript/packs/components/header/navigation/Navigation.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import NavigationItem from "./NavigationItem";
+import { useMousePosition } from "../../../../custom_hooks/useMousePosition";
 
 export default function Navigation(props) {
+	const mousePosition = useMousePosition();
+
 	const navigationItems = [
 		{
 			title: "Home"
@@ -26,12 +29,22 @@ export default function Navigation(props) {
 		}
 	];
 
+	function handleMouseOver(event) {
+		console.log(mousePosition);
+
+	}
+
+	const navigationStyle = {
+
+	};
+
 	return (
 		<div
 			className={
 				"navigation-menu-container " +
 				(props.navigationActive ? "active" : "")
 			}
+			onMouseOver={handleMouseOver}
 		>
 			<div className="navigation-items-container">
 				{navigationItems.map((item, index) => {
