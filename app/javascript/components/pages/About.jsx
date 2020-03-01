97dfc4f52b1da511ee6607e7935a9a377d88173f
Zach Mumbauer <mumbeezy@gmail.com>
2020-03-01 00:15:13 -0500

    changed about page wording

--- app/javascript/components/pages/About.jsx
+++ app/javascript/components/pages/About.jsx
@@ -7,16 +7,15 @@ export default function About(props) {
 			<p>
 				My life’s aspiration is to leverage technology and socially conscientious business practices to create a lasting, positive impact for as many people as possible.
 			</p>
-			<h1>So far, I...</h1>
-			<p>have visited all 50 states</p>
-			<p>am an Eagle Scout</p>
-			<p>play guitar, piano, and percussion</p>
-			<p>sang with a vocal group in Carnegie Hall</p>
-			<p>have worked with four different start-ups</p>
-			<p>can pour a swan in your latte</p>
-			<p>learn new things very quickly</p>
-			<p>completed an EMT-B course</p>
-			<p>have a motorcycle license</p>
+			<h1>Fun facts...</h1>
+			<p>I have visited all 50 states</p>
+			<p>I am an Eagle Scout</p>
+			<p>I play guitar, piano, and percussion</p>
+			<p>I sang with a vocal group in Carnegie Hall</p>
+			<p>I have worked with four different start-ups</p>
+			<p>I can pour a swan in your latte</p>
+			<p>I completed an EMT-B course</p>
+			<p>I have a motorcycle license</p>
 		</div>
 	);
 }
