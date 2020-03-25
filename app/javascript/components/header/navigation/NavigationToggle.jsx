61490685a47a0740e86f233dd6a13a5cf52b5491
zachm1995 <zachmumbauer@gmail.com>
2020-03-25 12:40:26 -0400

    removed scrollbar, updated header in scss toolkit, fixed navtoggle color on navopen

--- app/javascript/components/header/navigation/NavigationToggle.jsx
+++ app/javascript/components/header/navigation/NavigationToggle.jsx
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from "react";
 
 export default function NavigationToggle(props) {
 	const [currentColor, setCurrentColor] = useState("purple");
+
 	useEffect(() => {
-		switch (props.backgroundColor) {
-			case 'purple-gradient':
-				return setCurrentColor("white");
-			case 'ele-gradient':
-				return setCurrentColor("#6E8387");
-			default:
-				return setCurrentColor("white");
+		if (props.navigationActive) {
+			return setCurrentColor("red");
+		} else {
+			switch (props.backgroundColor) {
+				case "purple-gradient":
+					return setCurrentColor("white");
+				case "ele-gradient":
+					return setCurrentColor("#6E8387");
+				default:
+					return setCurrentColor("white");
+			}
 		}
-	}, [props.backgroundColor]);
+	}, [props.backgroundColor, props.navigationActive]);
 
 	return (
 		<div
@@ -22,15 +27,21 @@ export default function NavigationToggle(props) {
 			onPointerEnter={() => props.handleHoverableElement(true)}
 			onPointerLeave={() => props.handleHoverableElement(false)}
 		>
-			<span style={{
-				backgroundColor: currentColor,
-			}}></span>
-			<span style={{
-				backgroundColor: currentColor,
-			}}></span>
-			<span style={{
-				backgroundColor: currentColor,
-			}}></span>
+			<span
+				style={{
+					backgroundColor: currentColor
+				}}
+			></span>
+			<span
+				style={{
+					backgroundColor: currentColor
+				}}
+			></span>
+			<span
+				style={{
+					backgroundColor: currentColor
+				}}
+			></span>
 		</div>
 	);
 }
