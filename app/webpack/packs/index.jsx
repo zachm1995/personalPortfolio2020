a56481c78dcfe69e9fe37bdd1334e508d6272358
zachm1995 <zachmumbauer@gmail.com>
2020-08-20 15:55:08 -0400

    Renamed Javacsript folder to Webpack in anticipation of using webpack to manage styles and assets.
    Switched names of root react element and app container, changed import in the rails view file.
    
    Updated webpack config file to reflect directory change.

--- app/webpack/packs/index.jsx
+++ app/webpack/packs/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import PropTypes from "prop-types";
-import Index from "../components/Index";
+import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
 	ReactDOM.render(
 		<Router>
-			<Route path='/' component={Index} />
+			<Route path='/' component={App} />
 		</Router>,
 		document.body.appendChild(document.createElement("div"))
 	);
